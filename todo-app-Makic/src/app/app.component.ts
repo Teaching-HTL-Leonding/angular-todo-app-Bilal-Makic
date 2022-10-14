f8b0fd5dfffa7ccaca414dfd5de532d3d28a541d
Bilal Makic <b.makic@HTBLALeonding.onmicrosoft.com>
2022-10-14 10:46:48 +0200

    app created

--- todo-app-Makic/src/app/app.component.ts
+++ todo-app-Makic/src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+
+class Person{
+  constructor(
+    public name: string,
+    public description: string,
+    public toDo = 'smth'
+  ){}
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'todo-app-Makic';
+  public person: Person
+
+  constructor(){
+    this.person = new Person("John Doe", "Use the washing machine")
+  }
+
 }
